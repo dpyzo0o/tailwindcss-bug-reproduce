0cb317541a0337f8e309e1055e2680386b689143
dpyzo0o <yang.xu940121@outlook.com>
2020-07-16 10:52:20 +0800

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="prose mx-auto text-center">
+      <code>inline code</code>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      typography: {
+        default: {
+          css: {
+            code: {
+              '&::before': {
+                content: '""',
+              },
+              '&::after': {
+                content: '""',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/typography')],
+}
